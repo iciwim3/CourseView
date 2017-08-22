75d9dd8bf9ccacd0211266f487a2709fd25139a7
SKE <appybuildmore@gmail.com>
2017-08-22 10:44:01 -0500

    1. Embeded VC with Nav. Controller. 2. Created custom color for Nav. Bar. 3. Added Nav. Title with custom font. 4. Created a few Swift classes for the Model info. 5. Created a Swift class for the custom VC. 6. Deleted default VC. 7. Grouped files into MVC. 8. Added image assets to buttons.

---
+++ CourseView/Controller/CoursesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CoursesViewController.swift
+//  CourseView
+//
+//  Created by Sain-R Edwards Jr. on 8/22/17.
+//  Copyright © 2017 Appybuildmore. All rights reserved.
+//
+
+import UIKit
+
+class CoursesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
